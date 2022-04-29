eb56413a69acb402e6f6f54285038add89d96324
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-30 08:41:12 +1000

    Use monorepo SwiftGtk

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "HelloCairoGtk",
     dependencies: [
-        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk4")),
+        .package(url: "https://github.com/rhx/SwiftGtk.git", branch: "gtk4-monorepo"),
     ],
     targets: [
-        .target(name: "HelloCairoGtk", dependencies: ["Gtk"]),
+        .executableTarget(name: "HelloCairoGtk", dependencies: [
+            .product(name: "Gtk", package: "SwiftGtk"),
+        ]),
     ]
 )
